8f66e1a61ac1c6abc776cb7b18ab58229698706b
vga <vga@usb.lan>
2026-04-13 16:10:53 +0530

    clean render test app

--- app.py
+++ app.py
@@ -1,59 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
-from transformers import pipeline
 
-app = FastAPI(title="LLMOps RAG API")
-
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-
-db = FAISS.load_local(
-    "faiss_index",
-    embeddings,
-    allow_dangerous_deserialization=True
-)
-
-retriever = db.as_retriever(search_kwargs={"k": 2})
-
-llm = pipeline(
-    "text2text-generation",
-    model="google/flan-t5-base"
-)
+app = FastAPI(title="LLMOps RAG API - Test Mode")
 
 class Query(BaseModel):
     question: str
 
 @app.get("/")
 def home():
-    return {"status": "LLM RAG API running"}
+    return {
+        "status": "running",
+        "message": "FastAPI is deployed successfully on Render"
+    }
 
 @app.post("/chat")
 def chat(query: Query):
-
-    docs = retriever.invoke(query.question)
-    context = "\n".join([d.page_content for d in docs])
-
-    prompt = f"""
-Answer using only context:
-
-Context:
-{context}
-
-Question:
-{query.question}
-
-Answer:
-"""
-
-    result = llm(prompt, max_length=120, do_sample=False)
-
     return {
         "question": query.question,
-        "answer": result[0]["generated_text"]
+        "answer": "Render deployment is working (FAISS + LLM temporarily disabled for test)"
     }
-
-
-
